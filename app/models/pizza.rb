d841376d95ab33f03bc80a35e9c9cec02ff116ba
tid213 <tid2136141@gmail.com>
2024-02-14 16:56:35 -0700

    added more info to readme

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -4,15 +4,21 @@ class Pizza < ApplicationRecord
     validates :name, presence: true, :uniqueness => true,
                     length: { minimum: 1 }
 
-    validate :unique_toppings_combination
+    validate :unique_toppings_combination, on: :create
+
 
     private
     def unique_toppings_combination
-    existing_pizza = Pizza.joins(:toppings).where(toppings: { id: self.topping_ids }).where.not(id: self.id).first
+        existing_pizza = Pizza.joins(:pizza_toppings)
+        .where(pizza_toppings: { topping_id: topping_ids.sort })
+        .group(:id)
+        .having('COUNT(pizza_toppings.topping_id) = ?', toppings.size)
+        .select { |pizza| (pizza.toppings.pluck(:id).sort - topping_ids.sort).empty? }
+        .first
 
-    if existing_pizza
-      errors.add(:base, "Another pizza with the same toppings already exists.")
+        if existing_pizza.present?
+            errors.add(:base, "A pizza with the same toppings already exists.")
+        end
     end
-  end
 
 end
